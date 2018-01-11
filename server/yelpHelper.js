66cba5333176e6a1d74af8f4e5f798020e39c266
Hack Reactor Student <student@hackreactor.com>
2018-01-11 16:24:10 -0500

    working on if statements for yelp helper

--- server/yelpHelper.js
+++ server/yelpHelper.js
@@ -1,12 +1,14 @@
 var request = require('request');
 var saver = require('../database-mongo/index.js');
 
-var yelpGetter = function(term, long, lat, callback) {
-  var options = {
-    url: 'https://api.yelp.com/v3/businesses/search?term=' + term + '&latitude=' + lat + '&longitude=' + long + '&limit=9&sort_by=distance&open_now=true',
-    headers: {
-      'Authorization': `Bearer ${process.env._apikey}`
+var yelpGetter = function(temp, icon, long, lat, callback) {
+
+  if (temp < 30) {
+    options.url = `https://api.yelp.com/v3/businesses/search?term=fireplace&latitude=${lat}&longitude=${long}&limit=10&sort_by=distance&open_now=true&categories=bars`
     }
+
+  if (temp > 30 && temp < 50)
+  options.url = `https://api.yelp.com/v3/businesses/search?term=dive_bar&latitude=${lat}&longitude=${long}&limit=10&sort_by=distance&open_now=true&categories=bars`
   }  
 
   request(options, function(err, req, body) {
