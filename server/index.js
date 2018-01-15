cdb06c92e3a90b61da5998be0aee1740440bbc0c
Bill Eichhold <wjeichhold@gmail.com>
2018-01-14 21:21:53 -0500

    update port

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.post('/weather', function(req, res) {
 })
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('listening on port 3000!');
 });
 
