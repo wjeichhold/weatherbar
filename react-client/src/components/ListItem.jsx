0de3ad31174b7c5a29a9909ceabdf2ed47c33610
Bill Eichhold <wjeichhold@gmail.com>
2018-01-15 11:13:18 -0500

    remove yes button, add bootstrap dependency

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const ListItem = (props) => (
   <div>
     <img src={props.item.pictureUrl}/>
-    <button type="button" className="btn btn-yes">Into it!</button>
     <button type="button" className="btn btn-no">What else you got?</button>
   </div>
 )
